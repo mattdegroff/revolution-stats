cef5a1cb499c4905d41dca7a386284a3963b7a3a
mattdegroff <30853606+mattdegroff@users.noreply.github.com>
2018-05-10 23:28:17 -0400

    updating lead leaders

--- index.php
+++ index.php
@@ -89,6 +89,24 @@ include_once("connect.php");
           xmlhttp.send();
         }
 
+        function lead() {
+          if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+          } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+          }
+
+          xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+              document.getElementById("content").innerHTML = xmlhttp.responseText;
+            }
+          }
+          xmlhttp.open("GET","lead.php",true);
+          xmlhttp.send();
+        }
+
         home();
     </script>
     <title>LI Revolution</title>
@@ -170,6 +188,9 @@ include_once("connect.php");
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#" onclick="results()">Results</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" onclick="lead()">Stat Leaders</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Roster</a>
