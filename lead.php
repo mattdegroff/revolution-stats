acae980cae1202b67d4b14c0618bf69cf8388dbc
mattdegroff <30853606+mattdegroff@users.noreply.github.com>
2018-05-10 22:16:26 -0400

    updating lead leaders

--- lead.php
+++ lead.php
@@ -61,7 +61,7 @@ function qual() {
 }
 
 function avg() {
-  $sql = "select name, ab, (singles+doubles+triples+hr)/ab as ba from qual order by ba desc, ab";
+  $sql = "select name, (singles+doubles+triples+hr)/ab as ba from qual order by ba desc";
   $result = $conn->query($sql);
   if ($result) {
     while($row = $result->fetch_assoc()){
