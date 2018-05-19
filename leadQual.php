4120de60c5d09d21e7ca5ae82bb5ba78057210df
mattdegroff <30853606+mattdegroff@users.noreply.github.com>
2018-05-19 01:10:51 -0400

    pa/games file

---
+++ leadQual.php
@@ -0,0 +1,32 @@
+<?php
+include_once("connect.php");
+$current = 2018;
+
+$sql = "select `code` from players";
+$result = $conn->query($sql);
+  if ($result) {
+    while($row = $result->fetch_assoc()){
+        $code = $row['code'];
+        $name = $row['player'];
+        $pa = 0;
+
+        $sql = "select sum(ab), sum(sac), sum(walk) from ".$code." where year = ".$current;
+        $result1 = $conn->query($sql);
+        if ($result1) {
+          while($row1 = $result1->fetch_assoc()){
+            $pa = $row1['sum(ab)'] + $row1['sum(walk)'] + $row1['sum(sac)'];
+          }
+        }
+
+        $sql = "select count(*) from results where year = ".$current." and inning > 0";
+        $result1 = $conn->query($sql);
+        if ($result1) {
+          while($row1 = $result1->fetch_assoc()){
+            $games = $row1['count(*)'];
+
+          }
+        }
+
+        echo $code." - ".$pa/$games."<br>";
+      }
+    }
