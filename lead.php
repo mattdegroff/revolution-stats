b5213adb751299254c619c7a22583f0290e623f4
mattdegroff <30853606+mattdegroff@users.noreply.github.com>
2018-06-16 11:00:03 -0400

    debug

--- lead.php
+++ lead.php
@@ -1,6 +1,5 @@
 <?php
   include_once('leadPHP.php');
-  run();
  ?>
 
  <style>
--- leadPHP.php
+++ leadPHP.php
@@ -13,55 +13,53 @@ $sac = 0;
 $bb = 0;
 $k = 0;
 
-function run() {
-  $sql = "delete from qual";
-  $result = $conn->query($sql);
+$sql = "delete from qual";
+$result = $conn->query($sql);
 
-  $sql = "select player, `code` from players";
-  $result = $conn->query($sql);
-    if ($result) {
-      while($row = $result->fetch_assoc()){
-          $code = $row['code'];
-          $name = $row['player'];
-          $pa = 0;
+$sql = "select player, `code` from players";
+$result = $conn->query($sql);
+  if ($result) {
+    while($row = $result->fetch_assoc()){
+        $code = $row['code'];
+        $name = $row['player'];
+        $pa = 0;
 
-          $sql = "select sum(ab), sum(runs), sum(singles), sum(doubles), sum(triples), sum(hr), sum(rbi), sum(sac), sum(walk), sum(k) from ".$code." where year = ".$current;
-          $result1 = $conn->query($sql);
-          if ($result1) {
-            while($row1 = $result1->fetch_assoc()){
-              $pa = $row1['sum(ab)'] + $row1['sum(walk)'] + $row1['sum(sac)'];
-              $ab = $row1['sum(ab)'];
-              $r = $row1['sum(runs)'];
-              $sin = $row1['sum(singles)'];
-              $doub = $row1['sum(doubles)'];
-              $trip = $row1['sum(triples)'];
-              $hr = $row1['sum(hr)'];
-              $rbi = $row1['sum(rbi)'];
-              $sac = $row1['sum(sac)'];
-              $bb = $row1['sum(walk)'];
-              $k = $row1['sum(k)'];
-            }
-          }
-
-          $sql = "select count(*) from results where year = ".$current." and inning > 0";
-          $result1 = $conn->query($sql);
-          if ($result1) {
-            while($row1 = $result1->fetch_assoc()){
-              $games = $row1['count(*)'];
-
-            }
-          }
-
-          if ($pa/$games >= 2.25) {
-                $sql = "insert into qual (name, code, ab, runs, singles, doubles, triples, hr, rbi, sac, walk, k, qualify) values ('".$name."', '".$code."', ".$ab.", ".$r.", ".$sin.", ".$doub.", ".$trip.", ".$hr.", ".$rbi.", ".$sac.", ".$bb.", ".$k.", 1)";
-                $result2 = $conn->query($sql);
-          } else if ($games > 0){
-                $sql = "insert into qual (name, code, ab, runs, singles, doubles, triples, hr, rbi, sac, walk, k, qualify) values ('".$name."', '".$code."', ".$ab.", ".$r.", ".$sin.", ".$doub.", ".$trip.", ".$hr.", ".$rbi.", ".$sac.", ".$bb.", ".$k.", 0)";
-                $result2 = $conn->query($sql);
+        $sql = "select sum(ab), sum(runs), sum(singles), sum(doubles), sum(triples), sum(hr), sum(rbi), sum(sac), sum(walk), sum(k) from ".$code." where year = ".$current;
+        $result1 = $conn->query($sql);
+        if ($result1) {
+          while($row1 = $result1->fetch_assoc()){
+            $pa = $row1['sum(ab)'] + $row1['sum(walk)'] + $row1['sum(sac)'];
+            $ab = $row1['sum(ab)'];
+            $r = $row1['sum(runs)'];
+            $sin = $row1['sum(singles)'];
+            $doub = $row1['sum(doubles)'];
+            $trip = $row1['sum(triples)'];
+            $hr = $row1['sum(hr)'];
+            $rbi = $row1['sum(rbi)'];
+            $sac = $row1['sum(sac)'];
+            $bb = $row1['sum(walk)'];
+            $k = $row1['sum(k)'];
           }
         }
-    }
-}
+
+        $sql = "select count(*) from results where year = ".$current." and inning > 0";
+        $result1 = $conn->query($sql);
+        if ($result1) {
+          while($row1 = $result1->fetch_assoc()){
+            $games = $row1['count(*)'];
+
+          }
+        }
+
+        if ($pa/$games >= 2.25) {
+              $sql = "insert into qual (name, code, ab, runs, singles, doubles, triples, hr, rbi, sac, walk, k, qualify) values ('".$name."', '".$code."', ".$ab.", ".$r.", ".$sin.", ".$doub.", ".$trip.", ".$hr.", ".$rbi.", ".$sac.", ".$bb.", ".$k.", 1)";
+              $result2 = $conn->query($sql);
+        } else if ($games > 0){
+              $sql = "insert into qual (name, code, ab, runs, singles, doubles, triples, hr, rbi, sac, walk, k, qualify) values ('".$name."', '".$code."', ".$ab.", ".$r.", ".$sin.", ".$doub.", ".$trip.", ".$hr.", ".$rbi.", ".$sac.", ".$bb.", ".$k.", 0)";
+              $result2 = $conn->query($sql);
+        }
+      }
+  }
 
 function avg() {
   global $conn;
