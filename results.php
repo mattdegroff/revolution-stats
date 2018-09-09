5fc730196ac1da4e6ea2ba55c898b7c1bab96702
mattdegroff <30853606+mattdegroff@users.noreply.github.com>
2018-09-08 21:37:45 -0400

    debug

--- results.php
+++ results.php
@@ -109,7 +109,7 @@ table {
     <ul class="nav flex-column nav-pills nav-justified">
       <?php
       $i=0;
-      $sql = "select distinct league from results order by league desc";
+      $sql = "select leagues from leagueList order by id desc ";
       $result = $conn->query($sql);
       if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()){
