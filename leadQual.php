4df5f2ae970aab569e60f594339bd2ca042b2c2c
mattdegroff <30853606+mattdegroff@users.noreply.github.com>
2018-09-08 23:42:26 -0400

    debug

--- leadQual.php
+++ leadQual.php
@@ -1,6 +1,12 @@
 <?php
 include_once("connect.php");
-$current = 2018;
+$sql = "select active from activeSeason";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $current = $row['active'];
+  }
+}
 ?>
 <p>Must be at least 2.25 to qualify</p>
 <table>
@@ -27,7 +33,7 @@ $result = $conn->query($sql);
         $name = $row['player'];
         $pa = 0;
 
-        $sql = "select sum(ab), sum(sac), sum(walk) from ".$code." where year = ".$current;
+        $sql = "select sum(ab), sum(sac), sum(walk) from ".$code." where league = ".$current;
         $result1 = $conn->query($sql);
         if ($result1) {
           while($row1 = $result1->fetch_assoc()){
